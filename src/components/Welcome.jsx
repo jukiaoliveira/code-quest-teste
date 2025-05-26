559559644e26a396586823ded0bbf3e67295f17c
jukiaoliveira <jurafaoliveira@icloud.com>
2025-05-26 10:52:29 -0300

    feat: adicionar logo e estilizar página de boas-vindas

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
-import Quiz from "../img/quiz.svg"; 
+import Quiz from "../img/logo1.png"; 
 
 import "./Welcome.css";
 
@@ -12,8 +12,9 @@ const Welcome = () => {
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
       <img src={Quiz} alt="Início do Quiz" />
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
+      
     </div>
   );
 };
